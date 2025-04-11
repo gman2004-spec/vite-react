4b3011b5877b6c6986848823e67f84a3d1496d50
gman2004-spec <Viniciusbragastrujak@gmail.com>
2025-04-11 15:29:43 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,19 @@
 import './App.module.css'
-import { Btn } from './components/btn'
 import { Menu } from './components/menu'
 
 function App() {
   return(
     <>
-      <Menu s1="batata" s2="morango" s3="mandioca"/>
+      <Menu s1="SESSÃO 1" s2="SESSÃO 2" s3="SESSÃO 3"/>
       <main>
         <section id='s1'>
-          primeira
-          <Btn text='batata' func="#s2"/>
+         <h2>SESSÃO 1</h2>
         </section>
         <section id='s2'>
-          segunda <br />
-            <Btn text='mandioca' func="#s3"/>
+          <h2>SESSÃO 2</h2>
         </section>
         <section id='s3'>
-          terceira <br />
-            <Btn text='morango' func='#s1'/>
+          <h2>SESSÃO 3</h2>
           </section>
       </main>
     </>
