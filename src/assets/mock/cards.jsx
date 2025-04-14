1154c5e5d3643a1f62d4906373b3f036bd2689f0
gman2004-spec <Viniciusbragastrujak@gmail.com>
2025-04-14 18:22:12 -0300

    Update cards.jsx

--- src/assets/mock/cards.jsx
+++ src/assets/mock/cards.jsx
@@ -4,19 +4,4 @@ export const cards = [
     {
         text: "primeiro", img: photo1
     },
-    {
-        text: "segundo", img: photo1
-    },
-    {
-        text: "terceiro", img: photo1
-    },
-    {
-        text: "quarto", img: photo1
-    },
-    {
-        text: "quinto", img: photo1
-    },
-    {
-        text: "sexto", img: photo1
-    },
 ];
