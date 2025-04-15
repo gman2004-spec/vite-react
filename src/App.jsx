a60e65ed4e141a72cc1f7f8990656d7211bf1ed2
gman2004-spec <Viniciusbragastrujak@gmail.com>
2025-04-15 13:42:14 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { useState } from 'react'
 import { Calcs } from './Calcs.jsx'
 
 function App() {
-  const defaulPhoneNumber = "5541999999999"
+  const defaultPhoneNumber = "5541999999999"
   
   const [formData, setFormData] = useState({
     name: "",
@@ -24,7 +24,7 @@ function App() {
     const urlZAPZAP = `https://api.whatsapp.com/send?phone=${defaultPhoneNumber}&text=
     Nome:%20${name}%0D%A
     Email:%20${email}%0D%0A
-    Mensagem:%20{message}%0D%0A`;
+    Mensagem:%20${message}%0D%0A`;
 
     window.open(urlZAPZAP, "_blank")
   }
@@ -48,6 +48,7 @@ function App() {
             <input placeholder='Insira seu nome' type="text" id='name' name='name' value={formData.name} onChange={handleChange} required/><br /><br />
           <input placeholder='Insira seu email' type="email" id='email' name='email' value={formData.email} onChange={handleChange} required/><br /><br />
           <textarea placeholder='Insira mensagem' id='message' name='message' value={formData.message} onChange={handleChange} cols="30" rows="10" required></textarea>
+          <button onClick={handleZap}>Enviar mensagem</button>
           </section>
       </main>
 
@@ -56,5 +57,3 @@ function App() {
 }
 
 export default App
-
-
