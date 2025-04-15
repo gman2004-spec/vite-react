5b061a7ec9347c29f5b2eaca6b221a218ca390fe
gman2004-spec <Viniciusbragastrujak@gmail.com>
2025-04-15 16:10:34 -0300

    Update Req.jsx

--- src/Req.jsx
+++ src/Req.jsx
@@ -1,28 +1,40 @@
 import { useEffect, useState } from 'react'
 import style from './Req.module.css'
 import { api } from './api/api'
+import { Card } from './components/card'
+
+
 export default function Req(){
     const [data, setData] = useState([])
+    const [page, setPage] = useState("")
+    const [searchName, setSearchName] = useState("")
+
+    const [erro, setErro] = useState(false)
 
     useEffect(() => {
-        api.get(`/character`).then((response) => {
+        api.get(`/character/?page=${page}&name=${searchName}`).then((response) => {
             setData(response.data.results)
         }).catch((error) => {
             console.log(error)
+            setErro(true)
         })
-    }, [])
+    }, [page, searchName])
     console.log(data)
     return(
         <section className={style.wrapPage}>
-            <h1>Rick and Morty API</h1>
+            <h1 className={style.titleName}>Rick and Morty API</h1>
+            <input style={{padding: "10px", marginRight: "10px"}} type="text" placeholder='Digite uma página (1/42)' value={page} onChange={(e) => setPage(e.target.value)}/>
+            {erro && <p>Página não encontrada</p>}
+            <input style={{padding: "10px", marginRight: "10px"}} type="text" placeholder='' value={searchName} onChange={(e) => setSearchName(e.target.value)} />
+            <div className={style.wrapCards}>
             {data.map((item, index) =>{
                 return(
                     <div key={index}>
-                        <h5>{item.name}</h5>
-                        <img src={item.image} alt={item.name} />
+                       <Card name={item.name} image={item.image}/>
                     </div>
                 )
             })}
+            </div>
         </section>
     )
 }
