3e25eb3dbf7ccd561a490467e57267e27e79ed45
gman2004-spec <Viniciusbragastrujak@gmail.com>
2025-04-22 13:41:04 -0300

    Delete src/components/btn.jsx

--- src/components/btn.jsx
+++
@@ -1,9 +0,0 @@
-import './btn.module.css'
-
-export const Btn = ({text, func}) => {
-    return(
-        <button onClick={() => window.location.href=func}>
-            {text}
-        </button> 
-    )
-}
